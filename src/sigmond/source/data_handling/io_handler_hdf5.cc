cfafdfe129bf41ce31d6249cf7785c59351b1353
jmeneghini <meneghinijohn@gmail.com>
2025-06-10 18:37:15 -0400

    updated HDF5 support

--- src/sigmond/source/data_handling/io_handler_hdf5.cc
+++ src/sigmond/source/data_handling/io_handler_hdf5.cc
@@ -315,8 +315,8 @@ char IOHDF5Handler::query_obj(const std::string& objname) const
     if (exists<=0) return 'N';
     exists=H5Oexists_by_name(*cwdptr,tmpstring.c_str(),H5P_DEFAULT);
     if (exists<=0) return 'N';}
- H5O_info_t objinfo;
- herr_t errhandle=H5Oget_info_by_name(*cwdptr,objname.c_str(),&objinfo,H5P_DEFAULT);
+ H5O_info2_t objinfo;
+ herr_t errhandle=H5Oget_info_by_name(*cwdptr,objname.c_str(),&objinfo,H5O_INFO_ALL,H5P_DEFAULT);
  if (errhandle<0){
     check_for_herr_failure(errhandle,"query_obj failed");}
  if (objinfo.type==H5O_TYPE_GROUP) return 'G';
@@ -350,8 +350,8 @@ std::set<std::string> IOHDF5Handler::getAllDirNames() const
 void IOHDF5Handler::collect_data_names(hid_t loc_id, const std::string& path,
                     const char* name, set<std::string>& collected_names) const
 {
- H5O_info_t info;
- herr_t status = H5Oget_info(loc_id,&info);
+ H5O_info2_t info;
+ herr_t status = H5Oget_info(loc_id,&info,H5O_INFO_ALL);
  if (status<0){
     check_for_herr_failure(status,"collect_data_names failed");}
  if (info.type == H5O_TYPE_DATASET){
@@ -382,8 +382,8 @@ void IOHDF5Handler::collect_data_names(hid_t loc_id, const std::string& path,
 void IOHDF5Handler::collect_dir_names(hid_t loc_id, const std::string& path,
                     const char* name, set<std::string>& collected_dir_names) const
 {
- H5O_info_t info;
- herr_t status = H5Oget_info(loc_id,&info);
+ H5O_info2_t info;
+ herr_t status = H5Oget_info(loc_id,&info,H5O_INFO_ALL);
  if (status<0){
     check_for_herr_failure(status,"collect_dir_names failed");}
  if (info.type == H5O_TYPE_GROUP){
@@ -443,8 +443,8 @@ void IOHDF5Handler::collect_data_names_in_currdir(hid_t loc_id,
                                size_t(nsize), H5P_DEFAULT);
     hid_t next_id = H5Oopen(loc_id,nextname,H5P_DEFAULT);
     check_for_hid_failure(next_id," failure in collect_data_names");
-    H5O_info_t info;
-    herr_t status = H5Oget_info(next_id,&info);
+    H5O_info2_t info;
+    herr_t status = H5Oget_info(next_id,&info,H5O_INFO_ALL);
     if (status<0){
        check_for_herr_failure(status,"collect_data_names_in_currdir failed");}
     if (info.type == query_type){
